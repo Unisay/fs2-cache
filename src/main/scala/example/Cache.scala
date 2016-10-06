067317ffd6f8cf69b675f5b888603f3eb8dd7dff
Yuriy Lazarev <Yuriy.Lazarev@gmail.com>
2016-10-06 17:28:49 +0200

    Working solution

--- src/main/scala/example/Cache.scala
+++ src/main/scala/example/Cache.scala
@@ -3,38 +3,8 @@ package example
 import fs2._
 import fs2.util._
 import fs2.util.syntax._
+import scala.collection.mutable.ArrayBuffer
 import scala.language.higherKinds
-
-/**
-
-  I need to integrate read-through cache in the fs2 Stream.
-  Please consider the details:
-  ```scala
-
-  // given a key returns a task of value (fetcher)
-  def fetcher(key: Int): Task[String] = Task.delay {
-    println(s"Fetching a value for key $key")
-    key.toString
-  }
-
-  // A stream of keys
-  val keys: Stream[Task, Int] = Stream(1, 2, 2, 3, 2)
-
-  // A cache
-  val cache: Map[Int, String] = Map.empty
-
-  // A stream of values taken from cache or fetcher
-  val values: Stream[Task, String] = keys.(some steps that use fetcher?)
-
-  // Must print only:
-  // Fetching a value for key 1
-  // Fetching a value for key 2
-  // Fetching a value for key 3
-  values.runLog.unsafeRun mustEqual Vector("1", "2", "2", "3", "2")
-
-  ```
-
-  */
 
 object Cache {
 
@@ -42,10 +12,10 @@ object Cache {
   _.pull { handle =>
     handle.receive { case (chunk, h) =>
       val f2: (S, I) => F[(S, (S, O))] = (s: S, i: I) => {
-        f(s, i) map { case (newS, newO) => (newS, (newS, newO)) }
+        f(s, i) map { case t @ (newS, newO) => (newS, t) }
       }
-      val eval: Pull[F, (S, O), (S, Chunk[(S, O)])] = Pull.eval(chunkMapAccumulateEval(chunk)(init)(f2))
-      eval.flatMap { case (s, _) => _mapAccumulateEval0(s)(f2)(h) }
+      Pull.eval(chunkMapAccumulateEval(chunk)(init)(f2))
+        .flatMap { case (s, c) => Pull.output(c) >> _mapAccumulateEval0(s)(f2)(h) }
     }
   }
 
@@ -53,18 +23,12 @@ object Cache {
                                                     (f: (S,I) => F[(S,(S,O))])
                                                     (handle: Handle[F,I]): Pull[F,(S,O),Handle[F,I]] =
     handle.receive { case (chunk, h) =>
-      Pull.eval(chunkMapAccumulateEval(chunk)(init)(f)).flatMap {
-        case (s, c) =>
-          // Pull.output(c) >> ???
-          _mapAccumulateEval0(s)(f)(h)
-      }
+      Pull.eval(chunkMapAccumulateEval(chunk)(init)(f))
+        .flatMap { case (s, c) => Pull.output(c) >> _mapAccumulateEval0(s)(f)(h) }
     }
 
-  /** Simultaneously folds and maps this chunk, returning the output of the fold and the transformed chunk. */
   def chunkMapAccumulateEval[F[_]: Monad, S,B,A](chunk: Chunk[A])(s0: S)(f: (S,A) => F[(S,B)]): F[(S,Chunk[B])] = {
-    type BUF = collection.mutable.ArrayBuffer[B]
-    val buf = new collection.mutable.ArrayBuffer[B](chunk.size)
-    chunk.foldLeft(implicitly[Monad[F]].pure((s0, buf))) { (acc: F[(S, BUF)], a: A) =>
+    chunk.foldLeft(implicitly[Monad[F]].pure((s0, new ArrayBuffer[B](chunk.size)))) { (acc, a: A) =>
       for {
         t1 <- acc
         (s, bf) = t1
@@ -76,13 +40,11 @@ object Cache {
     }
   }
 
-  def inMemory[K, V](fetch: K => Task[V]): Pipe[Task, K, V] = _
+  def readThroughMap[K, V](fetch: K => Task[V]): Pipe[Task, K, V] = _
     .through {
-      mapAccumulateEval(Map.empty[K, V]) { (s: Map[K, V], k: K) =>
-          s.get(k)
-          .map(v => Task.now((s, v)))
-          .getOrElse(fetch(k).map(value => (s.updated(k, value), value)))
-        }
+      mapAccumulateEval(Map.empty[K, V]) { (s, k) =>
+        s get k map (v => Task.now((s, v))) getOrElse fetch(k).map(value => (s.updated(k, value), value))
       }
+    }
     .map { case (_, v) => v }
 }
--- src/test/scala/example/CacheSpec.scala
+++ src/test/scala/example/CacheSpec.scala
@@ -1,7 +1,6 @@
 package example
 
 import fs2._
-import example.Cache._
 import org.scalatest.MustMatchers
 import scala.collection.mutable.ListBuffer
 
@@ -9,14 +8,11 @@ class CacheSpec extends org.scalatest.FlatSpec with MustMatchers {
 
   "Read Through Cache" must "fetch elements it sees for the first time only" in {
     val effects = ListBuffer[Int]()
-    val fetcher = (i: Int) => Task.delay {
-      effects += i
-      i.toString
-    }
+    val fetcher = (i: Int) => Task.delay { effects += i; i.toString }
     val ids = List(1, 2, 2, 3, 2)
-    val values = Stream(ids: _*).through(inMemory(fetcher))
 
-    val results = values.runLog.unsafeRun()
+    val results = Stream(ids: _*).through(Cache.readThroughMap(fetcher)).runLog.unsafeRun()
+
     effects must contain theSameElementsInOrderAs List(1, 2, 3)
     results must contain theSameElementsInOrderAs List("1", "2", "2", "3", "2")
   }
